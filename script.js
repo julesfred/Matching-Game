41cdd851669240ca3b2893972c82ce5382672033
julesfred <jules.tf4@yahoo.com>
2017-11-08 14:27:42 -0500

    jquery animation for rotate

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $(function () {
   var parent = $(".container");
   var divs = parent.children();
@@ -5,3 +7,21 @@ $(function () {
     parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
   }
 });
+
+
+
+    
+    function flip() {
+        $(".card1").animate(
+            {rotation: 360},
+            {
+              duration: 500,
+              step: function(now) {
+                  $(this).css({"transform": "rotateY(" + now + "deg)"});
+                  $(this).css({"background-image": "url(images/grantchirpus.jpg)"});
+                  
+              }
+            }
+        );
+    }
+
