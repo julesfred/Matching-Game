c55b13cb9b0c745124e3d771e81ebcfcc097f1f6
shumcode <cshumylo@gmail.com>
2017-11-09 10:17:17 -0500

    fixed $ function

--- script.js
+++ script.js
@@ -31,9 +31,15 @@
 $(document).ready(function() {
   $("#start").on("click", function(event) {
     $(".container").css("display", "grid");
+
+    var parent = $(".container");
+    var divs = parent.children();
+    while (divs.length) {
+      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
   });
 
-  
+
   function flipit() {
     $(".back").removeClass("hide");
     $(".card").flip();
@@ -41,13 +47,9 @@ $(document).ready(function() {
   flipit();
 
 
-  $(function () {
-    var parent = $(".container");
-    var divs = parent.children();
-    while (divs.length) {
-      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-    }
-  });
+  
+    
+
 
   
 });
