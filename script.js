a3ae9cd19e1952c7e6e633078b5c3ca85d450972
shumcode <cshumylo@gmail.com>
2017-11-09 10:09:16 -0500

    we're getting somewhere

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-function startGame() {
+// $(document).ready(function() {
 
-jQuery(document).ready(function($){
+// function startGame() { 
+//   $(".container").css("display", "grid");
+// }
+
+// $(function () {
+//   var parent = $(".container");
+//   var divs = parent.children();
+//   while (divs.length) {
+//     parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+//   }
+// });
+
+
+// function flipit() {
+//     $(".back").removeClass("hide");
+//     $(".card").flip();
+// }
+
+
+
+// flipit();
+
+// function restartGame(){
+//   $(".container").flip();
+// }
+
+// });
+
+$(document).ready(function() {
+  $("#start").on("click", function(event) {
+    $(".container").css("display", "grid");
+  });
+
   
-$(".container").css("display", "grid");
-
-});
-
-$(function () {
-  var parent = $(".container");
-  var divs = parent.children();
-  while (divs.length) {
-    parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-  }
-
-});
-
-
-
-    // $(".card").on("click", function(){
-    //     $(this).animate(
-    //         {rotation: 360},
-    //         {
-    //           duration: 500,
-    //           step: function(now) {
-    //               $(this).css({"transform": "rotateY(" + now + "deg)"});
-    //               $(this).css({"background-image": "url('images/grantchirpus.jpg')"});
-    //               console.log(now);
-    //           }
-    //         }
-    //     );
-    // })
-
-
-function flipit() {
+  function flipit() {
     $(".back").removeClass("hide");
     $(".card").flip();
-}
+  }
+  flipit();
 
 
+  $(function () {
+    var parent = $(".container");
+    var divs = parent.children();
+    while (divs.length) {
+      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+  });
 
-flipit();
-
-}
-
-function restartGame(){
-  startGame();
-}
+  
+});
