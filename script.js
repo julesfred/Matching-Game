035a28a76a9ab685bd0a19722c0617d98ac14c5e
shumcode <cshumylo@gmail.com>
2017-11-10 09:04:22 -0500

    styles

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
-// $(document).ready(function() {
-//   $("#start").on("click", function(event) {
-//     $(".container").css("display", "grid");
-
-
-
-
-//     var parent = $(".container");
-//     var divs = parent.children();
-//     while (divs.length) {
-//       parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-//     }
-//   });
-
-
-// $(".card").on("click", function() {
-//   $(this).children(".front").css("-webkit-transform", "perspective(800px) rotateY(180deg)");
-//   $(this).children(".back").css("-webkit-transform", "perspective(800px) rotateY(0)");
-//   $(this).children(".front").toggleClass("change");
-//   $(this).children(".back").toggleClass("change2");
-// });
-
-
-
-// $(".restart").on("click", function(event) {
-//         var parent = $(".container");
-//         var divs = parent.children();
-//         while (divs.length) {
-//           parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-//         }
-//     });
-
-// });
-
-
 $(document).ready(function() {
   $("#start").on("click", function(event) {
     $(".container").css("display", "grid");
+
+
+
+
     var parent = $(".container");
     var divs = parent.children();
     while (divs.length) {
@@ -43,49 +12,61 @@ $(document).ready(function() {
     }
   });
 
-  $(".card").flip({ trigger: "click" });
+var cardOne = null;
+var cardTwo = null;
+var wins = 0;
 
-  var cardOne = null;
-  var cardTwo = null;
 
-  $(".card").on("click", function() {
-
-    $(".back").removeClass("hide");
-
-    if (cardOne === null) {
-      cardOne = $(this);
-      console.log(cardOne);
-    } else {
-      cardTwo = $(this);
-
-      if ( ($(cardOne).children(".back").attr("flipped") === $(cardTwo).children(".back").attr("flipped")) ) {
-        console.log("yay a match");
-        $(cardOne).children(".back").fadeOut(4000);
-        $(cardTwo).children(".back").fadeOut(4000);
-        cardOne = null;
-        cardTwo = null;
-      } else {
-        console.log("sucks");
-        $(cardOne).flip(false);
-        $(cardTwo).flip(false);
-        cardOne = null;
-        cardTwo = null;
+$(".card").on("click", function() {
+  if (cardOne === null) {
+    cardOne = $(this);
+    $(this).addClass("change");
+  } else {
+    cardTwo = $(this);
+    $(this).addClass("change");
+    if ((cardOne.children(".back").attr("flipped") === cardTwo.children(".back").attr("flipped")) ) {
+      console.log("yay a match");
+      cardTwo.delay(3000).fadeTo(1000, 0, function(){
+        $(this).removeClass("change");
+        $(this).css("visibility", "hidden");
+      });
+      if($(".card").hasClass("change")) {
+        cardOne.delay(3000).fadeTo(1000, 0, function(){
+          // $(this).removeClass("change");
+          $(this).css("visibility", "hidden");
+        });
       }
-
+      cardOne = null;
+      cardTwo = null;
+      wins++;
+      console.log(wins);
+    } else {
+      cardOne.delay(3000).queue(function(next) {
+        $(this).removeClass("change");
+      });
+      $(this).addClass("change").delay(3000).queue(function(next){
+        $(this).removeClass("change");
+        $(".card").removeClass("change");
+        next();
+      });
+      cardOne = null;
+      cardTwo = null;
     }
+  }
+  if (wins >= 6) {
+    // $(".card").off("click");
+    console.log("You win!");
+  }
+});
 
 
-  })
 
-
-
-  $(".restart").on("click", function(event) {
-    $(".back").css("backface-visibility", "visible");
-    $(".front").css("backface-visibility", "visible");
-    $(".card").flip(false);
-
-  });
-
-
+$(".restart").on("click", function(event) {
+        var parent = $(".container");
+        var divs = parent.children();
+        while (divs.length) {
+          parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+        }
+    });
 
 });
