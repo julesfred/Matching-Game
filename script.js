41191a06f43faf20f162c4122575c3362312427e
Anh mai <lyraducusin@gmail.com>
2017-11-10 11:54:56 -0500

    finally finish

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
+
+  //Prevent double clicks
   $("*").dblclick(function(e){
     e.preventDefault();
-
   });
+
+  //Function to start the game
   $("#start").on("click", function(event) {
     $(".container").css("display", "grid").animate({top: '100px'});
-
-
-
-
     var parent = $(".container");
     var divs = parent.children();
     while (divs.length) {
@@ -16,6 +15,7 @@ $(document).ready(function() {
     }
   });
 
+  //Restart game//
   $(".restart").on("click", function(){
     $(".wincontainer").css("display", "none");
     $(".card").removeClass("change").css("visibility", "visible");
@@ -26,48 +26,47 @@ $(document).ready(function() {
     }
   });
 
-var cardOne = null;
-var cardTwo = null;
-var wins = 0;
+  var cardOne = null;
+  var cardTwo = null;
+  var wins = 0;
 
 
-$(".card").on("click", function() {
-  if (cardOne === null) {
-    cardOne = $(this);
-    $(this).addClass("change");
-  } else {
-    cardTwo = $(this);
-    $(this).addClass("change");
-    if (($(cardOne).children(".back").attr("flipped") === $(cardTwo).children(".back").attr("flipped")) ) {
-      console.log("yay a match");
-      setTimeout(function(){
-        if($(".card").hasClass("change")){
-          $(".change").removeClass("change").css("visibility", "hidden");
-        }
-      }, 3000)
-      cardOne = null;
-      cardTwo = null;
-      wins++;
-      console.log(wins);
+  $(".card").on("click", function() {
+    //Sets card one = to the card you click on, flip the card
+    if (cardOne === null) {
+      cardOne = $(this);
+      $(this).addClass("change");
     } else {
-      setTimeout(function(){
-        if($(".card").hasClass("change")){
-          $(".card").removeClass("change");
-        }
-      }, 3000)
-      cardOne = null;
-      cardTwo = null;
+      cardTwo = $(this);
+      $(this).addClass("change");
+      //If the first card is equivalent to the second card then it removes both cards
+      if (($(cardOne).children(".back").attr("flipped") === $(cardTwo).children(".back").attr("flipped")) ) {
+        setTimeout(function(){
+          if($(".card").hasClass("change")){
+            $(".change").removeClass("change").css("visibility", "hidden");
+          }
+        }, 3000)
+
+        //Resetting value of cards so you can click another cards
+        cardOne = null;
+        cardTwo = null;
+        wins++;
+
+        // If cards don't match this runs and flips them back
+      } else {
+        setTimeout(function(){
+          if($(".card").hasClass("change")){
+            $(".card").removeClass("change");
+          }
+        }, 3000)
+        cardOne = null;
+        cardTwo = null;
+      }
     }
-  }
-  if (wins >= 10) {
-    console.log("You win!");
-    $(".wincontainer").text("You won the game!");
-    wins = 0;
-  }
-});
-
-
-
-
-
+    //Win condition
+    if (wins >= 10) {
+      $(".wincontainer").text("You won the game!");
+      wins = 0;
+    }
+  });
 });
